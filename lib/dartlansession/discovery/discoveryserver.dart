41ecdfbe0b6324a0d47c2b420086cc10bdbe8519
fpuma <44748038+fpuma@users.noreply.github.com>
2026-02-24 21:22:39 +0100

    Exporting needed for types exposed in the API

--- lib/dartlansession/discovery/discoveryserver.dart
+++ lib/dartlansession/discovery/discoveryserver.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
 import 'dart:typed_data';
 
+export 'dart:io';
+export 'dart:typed_data';
+
 typedef MessageCallback =
     (bool, Uint8List) Function(
       InternetAddress address,
